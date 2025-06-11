fc3c3c9b1f1756222e15b89a1dcb199e4247446a
aurelie31000 <hameras.aurelie@gmail.com>
2025-06-11 09:35:01 +0200

    Ajout ou modification de palindrome.py

--- palindrome.py
+++ palindrome.py
@@ -1,31 +1,39 @@
 import datetime
 
 def est_palindrome(texte):
-    texte = ''.join(c.lower() for c in texte if c.isalnum())
-    return texte == texte[::-1]
+    texte_filtre = ''.join(c.lower() for c in texte if c.isalnum())
+    return texte_filtre == texte_filtre[::-1]
+
+def miroir(texte):
+    return texte[::-1]
+
+def salutation_bonjour():
+    heure = datetime.datetime.now().hour
+    if 5 <= heure < 18:
+        return "Bonjour !"
+    else:
+        return "Bonsoir !"
 
 def salutation_au_revoir():
     heure = datetime.datetime.now().hour
-    if 5 <= heure < 12:
-        return "Au revoir, bonne matinée !"
-    elif 12 <= heure < 18:
-        return "Au revoir, bon après-midi !"
-    elif 18 <= heure < 22:
-        return "Au revoir, bonne soirée !"
+    if 5 <= heure < 18:
+        return "Au revoir, bonne journée !"
     else:
-        return "Au revoir, bonne nuit !"
+        return "Au revoir, bonne soirée !"
 
 def main():
-    print("Bienvenue ! Saisissez un mot ou une phrase (ou tapez 'exit' pour quitter).")
+    print(salutation_bonjour())
+    print("Saisissez un mot ou une phrase (ou tapez 'exit' pour quitter).")
     while True:
         entree = input("> ")
         if entree.lower() == "exit":
             print(salutation_au_revoir())
             break
+        print(miroir(entree))
         if est_palindrome(entree):
-            print("Bien dit")
-        else:
-            print("Ce n'est pas un palindrome.")
+            print("Bien dit !")
 
 if __name__ == "__main__":
     main()
+
+
